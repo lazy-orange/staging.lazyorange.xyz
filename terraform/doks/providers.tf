b77dbab65f03f8cf459f22510536e575521d3300
Vitaly Markov <v.y.markov@gmail.com>
2019-12-07 08:17:02 +0000

    chore(ci/cd): Add s3 remote state

--- terraform/doks/providers.tf
+++ terraform/doks/providers.tf
@@ -3,5 +3,7 @@ terraform {
 
   required_providers {
     digitalocean = "~> 1.11"
+    gitlab       = "~> 2.4"
+    kubernetes   = "~> 1.10"
   }
 }
---
+++ terraform/doks/remote-state.tf
@@ -0,0 +1,36 @@
+# In this case was used the terraform-aws-tfstate-backend module by CloudPosse to create a AWS S3 backend.
+# You can can create an s3 backend in other way.
+#
+# References:
+# - https://github.com/cloudposse/terraform-aws-tfstate-backend
+# - https://github.com/cloudposse/terraform-aws-tfstate-backend#usage
+# - https://github.com/cloudposse/terraform-aws-tfstate-backend/releases
+
+module "terraform_state_backend" {
+  source        = "git::https://github.com/cloudposse/terraform-aws-tfstate-backend.git?ref=0.9.0"
+  namespace     = "lo"
+  stage         = "dev"
+  region        = "eu-central-1"
+  name          = "terraform"
+  attributes    = ["state"]
+  force_destroy = true
+}
+
+// output "terraform_backend_config" {
+//   value = module.terraform_state_backend.terraform_backend_config
+// }
+
+terraform {
+  required_version = ">= 0.12.2"
+
+  backend "s3" {
+    region         = "eu-central-1"
+    bucket         = "lo-dev-terraform-state"
+    key            = "terraform.tfstate"
+    dynamodb_table = "lo-dev-terraform-state-lock"
+    profile        = ""
+    role_arn       = ""
+    encrypt        = "true"
+  }
+}
+
