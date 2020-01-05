28707577d21985fedffe5739d29d2e82340e5820
Vitaly Markov <v.y.markov@gmail.com>
2020-01-05 13:42:44 +0000

    Update terraform/doks/remote-state.tf

--- terraform/doks/remote-state.tf
+++ terraform/doks/remote-state.tf
@@ -5,29 +5,37 @@
 # - https://github.com/cloudposse/terraform-aws-tfstate-backend
 # - https://github.com/cloudposse/terraform-aws-tfstate-backend#usage
 # - https://github.com/cloudposse/terraform-aws-tfstate-backend/releases
+# ```bash
+# $ cd ./terraform/doks
+# $ cp remote-state.tf.example remote-state.tf
+# $ terraform init
+# $ terraform apply -var-file fra1.tfvars -target=module.terraform_state_backend
+# $ terraform output terraform_backend_config >> remote-state.tf
+# $ terraform init
+#```
 
-// module "terraform_state_backend" {
-//   source        = "git::https://github.com/cloudposse/terraform-aws-tfstate-backend.git?ref=0.9.0"
-//   namespace     = "lo"
-//   stage         = "dev"
-//   region        = "eu-central-1"
-//   name          = "terraform"
-//   attributes    = ["state", "digitalocean", "do"]
-//   force_destroy = true
-// }
+module "terraform_state_backend" {
+  source        = "git::https://github.com/cloudposse/terraform-aws-tfstate-backend.git?ref=0.9.0"
+  namespace     = "lo"
+  stage         = "dev"
+  region        = "eu-central-1"
+  name          = "terraform"
+  attributes    = ["state", "do", "gitlab"]
+  force_destroy = true
+}
 
-// output "terraform_backend_config" {
-//   value = module.terraform_state_backend.terraform_backend_config
-// }
+output "terraform_backend_config" {
+  value = module.terraform_state_backend.terraform_backend_config
+}
 
 terraform {
   required_version = ">= 0.12.2"
 
   backend "s3" {
     region         = "eu-central-1"
-    bucket         = "lo-dev-terraform-state-digitalocean-do"
+    bucket         = "lo-dev-terraform-state-do-gitlab"
     key            = "terraform.tfstate"
-    dynamodb_table = "lo-dev-terraform-state-digitalocean-do-lock"
+    dynamodb_table = "lo-dev-terraform-state-do-gitlab-lock"
     profile        = ""
     role_arn       = ""
     encrypt        = "true"
