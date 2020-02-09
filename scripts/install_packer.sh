4a61322ee300b0bfcc5799b5f16f466ab1a95ae2
Vitaly Markov <v.y.markov@gmail.com>
2020-02-09 17:01:23 +0000

    feat(packer): Add packer config to build AWS AMI image with all needed tools

---
+++ scripts/install_packer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o pipefail
+
+export PACKER_VERSION=${PACKER_VER:-"1.5.1"}
+export WORKDIR=/tmp/packer
+
+rm -rf $WORKDIR && mkdir -p $WORKDIR && cd $WORKDIR
+
+wget "https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip"
+unzip "packer_${PACKER_VERSION}_linux_amd64.zip"
+
+mv ./packer /usr/local/bin/packer
+chmod +x /usr/local/bin/packer
+
+rm -rf $WORKDIR
+
+packer version
