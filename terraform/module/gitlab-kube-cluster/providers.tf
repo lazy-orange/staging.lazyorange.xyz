db19f35fca2d8b4f9100eb33663921a70bcd6b43
Vitaly Markov <v.y.markov@gmail.com>
2020-02-23 11:06:17 +0200

    fix(terraform): Pin kubernetes provider version to 1.10.0 due issue (https://github.com/terraform-providers/terraform-provider-kubernetes/issues/759)

--- terraform/module/gitlab-kube-cluster/providers.tf
+++ terraform/module/gitlab-kube-cluster/providers.tf
@@ -1,4 +1,7 @@
 provider "kubernetes" {
+  version                = "1.10.0"
+  load_config_file         = false
+
   host                   = var.kubernetes_endpoint
   token                  = var.kubernetes_token
   cluster_ca_certificate = base64decode(var.kubernetes_ca_cert)
